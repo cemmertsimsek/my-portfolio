ffbcba34d86c8c453648e63afad82f5a529fac0d
cem <cemsimseksm@gmail.com>
2022-12-14 18:20:01 +0300

    floating nav completed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,53 @@
 import './nav.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineUser } from 'react-icons/ai';
+import { GiSkills } from 'react-icons/gi';
+import { TbMessageCircle } from 'react-icons/tb';
+import { AiOutlineFundProjectionScreen } from 'react-icons/ai';
+import { useState } from 'react';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState('#');
+
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav('#')}
+        className={activeNav === '#' ? 'active' : ''}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#skills"
+        onClick={() => setActiveNav('#skills')}
+        className={activeNav === '#skills' ? 'active' : ''}
+      >
+        <GiSkills />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav('#portfolio')}
+        className={activeNav === '#portfolio' ? 'active' : ''}
+      >
+        <AiOutlineFundProjectionScreen />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}
+      >
+        <TbMessageCircle />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
