daa3a9f2c2f124b13cf7714baaae956098b43b64
cem <cemsimseksm@gmail.com>
2022-12-14 18:59:09 +0300

    initial commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,30 @@
 import './about.css';
+import ME1 from '../../assets/me.png';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME1} alt="AboutImage" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
