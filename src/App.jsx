d7ac79ba74bc249f225dba04da61e029432d0671
cem <cemsimseksm@gmail.com>
2022-12-14 15:46:32 +0300

    calling components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import './App.css';
+import React from 'react';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
