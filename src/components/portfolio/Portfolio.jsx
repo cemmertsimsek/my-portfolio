852d7f7dba65f24709dfcd58166b77e052a23aed
cem <cemsimseksm@gmail.com>
2022-12-15 17:57:58 +0300

    portfolio structure created for styling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,39 @@
 import './portfolio.css';
+import IMG1 from '../../assets/portfolio1.jpg';
+// import IMG2 from '../../assets/portfolio2.jpg';
+// import IMG3 from '../../assets/portfolio3.jpg';
+// import IMG4 from '../../assets/portfolio4.jpg';
+// import IMG5 from '../../assets/portfolio5.jpg';
+// import IMG6 from '../../assets/portfolio6.jpg';
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="img1" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="GITHUB LINK HERE" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="LIVE PROJECT LINK HERE"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
