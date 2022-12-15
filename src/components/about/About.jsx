327f2a0cab109457e9d1fe59cfeb8db9a27d15b1
cem <cemsimseksm@gmail.com>
2022-12-15 17:57:34 +0300

    about page updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import './about.css';
 import ME1 from '../../assets/me.png';
+import { FaAward } from 'react-icons/fa';
 
 const About = () => {
   return (
@@ -11,16 +12,41 @@ const About = () => {
         <div className="about__me">
           <div className="about__me-image">
             <img src={ME1} alt="AboutImage" />
+            {/* buraya arka plani mavi fotograf */}
           </div>
         </div>
 
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">
-              <h5>Experience</h5>
+              <FaAward className="about__icon" />
+              <h5>Skills</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Skills</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Skills</h5>
               <small>3+ Years Working</small>
             </article>
           </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
+            labore praesentium repudiandae repellendus ratione porro laudantium
+            error? Deleniti optio ut impedit vero, sapiente repellat, ducimus
+            eaque obcaecati quia nobis non.
+          </p>
+
+          <a href="#contact" className="btn btn-primary btn-contact">
+            Contact Me
+          </a>
         </div>
       </div>
     </section>
