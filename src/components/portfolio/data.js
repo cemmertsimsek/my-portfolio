947bff12728e3f92e6f38078ddc40c7f4fc882b2
cem <cemsimseksm@gmail.com>
2022-12-19 18:23:26 +0300

    portfolio objects

---
+++ src/components/portfolio/data.js
@@ -0,0 +1,53 @@
+import IMG1 from '../../assets/IMG1.jpg';
+import IMG2 from '../../assets/IMG2.jpg';
+import IMG3 from '../../assets/IMG3.jpg';
+import IMG4 from '../../assets/IMG4.jpg';
+import IMG5 from '../../assets/IMG5.jpg';
+import IMG6 from '../../assets/IMG6.jpg';
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Metaversus',
+    github: 'https://github.com/cemmertsimsek/metaversus',
+    demo: 'https://metaversus-cemmertsimsek.vercel.app/',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'HooBank',
+    github: 'https://github.com/cemmertsimsek/bank-app',
+    demo: 'https://bank-app-cemmertsimsek.vercel.app/',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'GPT-3',
+    github: 'https://github.com/cemmertsimsek/gpt-3-frontend-website',
+    demo: 'https://gpt-3-frontend-website-cemmertsimsek.vercel.app/',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Blog-Website',
+    github: 'https://github.com/cemmertsimsek/Blog-Website',
+    demo: 'https://cemsimsekblogwebsite.cyclic.app/',
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'To-Do-List-v2',
+    github: 'https://github.com/cemmertsimsek/To-Do-List-v2',
+    demo: 'https://cemsimsektodolist.cyclic.app/',
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Newsletter-Signup',
+    github: 'https://github.com/cemmertsimsek/Newsletter-Signup',
+    demo: 'https://plum-concerned-bass.cyclic.app/',
+  },
+];
+
+export default data;
